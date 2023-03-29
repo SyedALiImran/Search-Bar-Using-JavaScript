c38a522412de98e293148693ea9425870dbbddcc
SyedAliImran <syed.ali.imran456@gmail.com>
2023-03-29 06:32:15 +0500

    search functionality

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-
 function search(){
 
     let filterKeyword = document.getElementById("searchKeyword").value.toUpperCase();
     
     
+
+    let tabel = document.getElementById("table");
+
+    let tableRow = tabel.getElementsByTagName("tr");
+
+    for(var i = 0; i<tableRow.length; i++){
+    let td = tableRow[i].getElementsByTagName("td")[0];
+    
+    if(td){
+        let textValue = td.textContent;
+
+        if(textValue.toUpperCase().indexOf(filterKeyword)>-1){
+            tableRow[i].style.display = "";
+        }else{
+            tableRow[i].style.display = "none";
+        }
+        
+    }
+    
+    }
+
 }
